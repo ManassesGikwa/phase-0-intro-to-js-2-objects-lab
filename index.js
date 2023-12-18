1de159e93b80d4b076f12e15f960b0bbf3984b2a
ManassesGikwa <manassesgikwa@gmail.com>
2023-12-18 18:16:15 +0300

    Completed Assignmet

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-// Write your solution in this file!
+// // // Write your solution in this file!
+
+
+// updateEmployeeWithKeyAndValue()
+// {
+//     const updatedEmployee={...employee,
+//         name: "John",
+//         streetAddress:"11th street"   
+//     }
+
+//     return updatedEmployee;
+// }
+//     const updatedObject = updateEmployeeWithKeyAndValue ();
+//     console.log(updatedObject);
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//       employee["name"] = "Manasses";
+//       employee["streetAddress"] = "5th NgongAvenue";
+//       return employee;
+// }
+    
+
+// function deleteFromEmployeeByKey(employee, key) {
+//     let newEmployee = {...employee};
+//     delete newEmployee[key];
+//     return newEmployee;
+// }
+  
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//    delete employee[key];
+//   return employee;
+// }
+
+let employee = {
+    name: "Manasses",
+    streetAddress: "5th NgongAvenue"
+  } 
+function updateEmployeeWithKeyAndValue () {
+   const updateEmployee = {...employee,
+    name:"Sam",
+    streetAddress:"11 Broadway"
+   }
+
+   return updateEmployee;
+}
+
+const updatedObject = updateEmployeeWithKeyAndValue ();
+
+console.log(updatedObject);
+
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee["name"] = "Sam";
+    employee["streetAddress"] = "12 Broadway"
+    return employee;
+  }
+  
+
+function deleteFromEmployeeByKey(employee, key) {
+  let newEmployee = {...employee};
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+
